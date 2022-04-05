2f5f8ab10a428db489af0701f71644120d470c8b
gustacaru <gustavoalmeida26@hotmail.com>
2022-04-05 18:40:05 -0300

    add new test-cases

--- src/tests/integration.test.js
+++ src/tests/integration.test.js
@@ -151,7 +151,52 @@ describe('Critérios de Elegibilidade de Cliente ', () => {
 
   });
 
-  describe('4 - Testa quando são informados critérios de elegibilidade inválidos para as categorias "classeDeConsumo" & "modalidadeTarifaria" ', () => {
+  describe('4 - Testa quando é informado critério de elegibilidade inválido para categoria "consumo mínimo" ', () => {
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/admittance')
+        .send({
+          "numeroDoDocumento": "24031738806",
+          "tipoDeConexao": "trifasico",
+          "classeDeConsumo": "industrial",
+          "modalidadeTarifaria": "branca",
+          "historicoDeConsumo": [
+            387,
+            176,
+            597,
+            279,
+            248,
+            573,
+            553,
+            439,
+            785,
+            416,
+            294,
+            459
+          ]
+        });
+    });
+
+    it('Deve retornar o status 200', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('A requisição deve retornar um json com a chave "elegivel" com o valor false', () => {
+      expect(response.body.elegivel).to.be.equals(false);
+    })
+
+    it('A requisição deve retornar um json com a chave "razoesInelegibilidade" e ela deve ser um array', () => {
+      expect(response.body.razoesInelegibilidade).to.be.a('array');
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array que contem o texto: "Consumo muito baixo para tipo de conexão"', () => {
+      expect(response.body.razoesInelegibilidade[0]).to.be.equals('Consumo muito baixo para tipo de conexão');
+    })
+
+  });
+
+  describe('5 - Testa quando são informados critérios de elegibilidade inválidos para as categorias "classeDeConsumo" & "modalidadeTarifaria" ', () => {
 
     before(async () => {
       response = await chai.request(server)
@@ -192,6 +237,162 @@ describe('Critérios de Elegibilidade de Cliente ', () => {
 
     it('A chave "razoesInelegibilidade" deve ser um array com comprimento de 2 itens', () => {
       expect(response.body.razoesInelegibilidade).to.have.a.lengthOf(2);
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array que contem os textos: "Classe de consumo não aceita" & "Modalidade tarifária não aceita"', () => {
+      expect(response.body.razoesInelegibilidade[0]).to.be.equals('Classe de consumo não aceita');
+      expect(response.body.razoesInelegibilidade[1]).to.be.equals('Modalidade tarifária não aceita');
+    })
+
+  });
+
+  describe('6 - Testa quando são informados critérios de elegibilidade inválidos para as categorias "classeDeConsumo" & "consumo mínimo" ', () => {
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/admittance')
+        .send({
+          "numeroDoDocumento": "24031738806",
+          "tipoDeConexao": "trifasico",
+          "classeDeConsumo": "rural",
+          "modalidadeTarifaria": "branca",
+          "historicoDeConsumo": [
+            387,
+            176,
+            597,
+            279,
+            248,
+            573,
+            553,
+            439,
+            785,
+            416,
+            294,
+            459
+          ]
+        });
+    });
+
+    it('Deve retornar o status 200', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('A requisição deve retornar um json com a chave "elegivel" com o valor false', () => {
+      expect(response.body.elegivel).to.be.equals(false);
+    })
+
+    it('A requisição deve retornar um json com a chave "razoesInelegibilidade" e ela deve ser um array', () => {
+      expect(response.body.razoesInelegibilidade).to.be.a('array');
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array com comprimento de 2 itens', () => {
+      expect(response.body.razoesInelegibilidade).to.have.a.lengthOf(2);
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array que contem os textos: "Consumo muito baixo para tipo de conexão"', () => {
+      expect(response.body.razoesInelegibilidade[0]).to.be.equals('Classe de consumo não aceita');
+      expect(response.body.razoesInelegibilidade[1]).to.be.equals('Consumo muito baixo para tipo de conexão');
+    })
+
+  });
+
+  describe('7 - Testa quando são informados critérios de elegibilidade inválidos para as categorias "modalidadeTarifaria" & "consumo mínimo" ', () => {
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/admittance')
+        .send({
+          "numeroDoDocumento": "24031766806",
+          "tipoDeConexao": "trifasico",
+          "classeDeConsumo": "residencial",
+          "modalidadeTarifaria": "verde",
+          "historicoDeConsumo": [
+            387,
+            176,
+            597,
+            279,
+            248,
+            573,
+            553,
+            439,
+            785,
+            416,
+            294,
+            459
+          ]
+        });
+    });
+
+    it('Deve retornar o status 200', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('A requisição deve retornar um json com a chave "elegivel" com o valor false', () => {
+      expect(response.body.elegivel).to.be.equals(false);
+    })
+
+    it('A requisição deve retornar um json com a chave "razoesInelegibilidade" e ela deve ser um array', () => {
+      expect(response.body.razoesInelegibilidade).to.be.a('array');
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array com comprimento de 2 itens', () => {
+      expect(response.body.razoesInelegibilidade).to.have.a.lengthOf(2);
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array que contem os textos: "Modalidade tarifária não aceita" & "Consumo muito baixo para tipo de conexão"', () => {
+      expect(response.body.razoesInelegibilidade[0]).to.be.equals('Modalidade tarifária não aceita');
+      expect(response.body.razoesInelegibilidade[1]).to.be.equals('Consumo muito baixo para tipo de conexão');
+    })
+
+  });
+
+  describe('8 - Testa quando são informados critérios de elegibilidade inválidos para as categorias "classeDeConsumo", "modalidadeTarifaria" & "consumo mínimo" ', () => {
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/admittance')
+        .send({
+          "numeroDoDocumento": "24031766806",
+          "tipoDeConexao": "trifasico",
+          "classeDeConsumo": "rural",
+          "modalidadeTarifaria": "azul",
+          "historicoDeConsumo": [
+            387,
+            176,
+            597,
+            279,
+            248,
+            573,
+            553,
+            439,
+            785,
+            416,
+            294,
+            459
+          ]
+        });
+    });
+
+    it('Deve retornar o status 200', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('A requisição deve retornar um json com a chave "elegivel" com o valor false', () => {
+      expect(response.body.elegivel).to.be.equals(false);
+    })
+
+    it('A requisição deve retornar um json com a chave "razoesInelegibilidade" e ela deve ser um array', () => {
+      expect(response.body.razoesInelegibilidade).to.be.a('array');
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array com comprimento de 2 itens', () => {
+      expect(response.body.razoesInelegibilidade).to.have.a.lengthOf(3);
+    })
+
+    it('A chave "razoesInelegibilidade" deve ser um array que contem os textos: "Classe de consumo não aceita", "Modalidade tarifária não aceita" & "Consumo muito baixo para tipo de conexão" ', () => {
+      expect(response.body.razoesInelegibilidade[0]).to.be.equals('Classe de consumo não aceita');
+      expect(response.body.razoesInelegibilidade[1]).to.be.equals('Modalidade tarifária não aceita');
+      expect(response.body.razoesInelegibilidade[2]).to.be.equals('Consumo muito baixo para tipo de conexão');
     })
 
   });
