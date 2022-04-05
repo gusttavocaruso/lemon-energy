143ad7e9ab12447ddb408a6a590a1cfc462d13fc
gustacaru <gustavoalmeida26@hotmail.com>
2022-04-05 13:23:33 -0300

    add new business rule to min consumption

--- src/api/utils/validationFunct.js
+++ src/api/utils/validationFunct.js
@@ -4,8 +4,8 @@ const errHandle = require('./errHendle');
 const consumptionClasses = ({ classeDeConsumo }) => {
   if (classeDeConsumo.includes('rural')
   || classeDeConsumo.includes('publico')) {
-      return false;
-    };
+    return false;
+  };
   return true;
 };
 
@@ -17,19 +17,53 @@ const modalityTax = ({ modalidadeTarifaria }) => {
   return true;
 };
 
+const minConsumption = ({ tipoDeConexao, historicoDeConsumo }) => {
+  const somaConsump = historicoDeConsumo.reduce((acc, consump) => acc + consump);
+  const avaregeConsump = somaConsump / historicoDeConsumo.length;
+  console.log(avaregeConsump);
+
+  if (tipoDeConexao === 'monofasico' && avaregeConsump < 400) return false; 
+  if (tipoDeConexao === 'bifasico' && avaregeConsump < 500) return false;
+  if (tipoDeConexao === 'trifasico' && avaregeConsump < 750) return false;
+
+  return true;
+}
+
 const eligibilityCriteria = (customerPayload) => {
-  const isOkClasses = consumptionClasses(customerPayload);
-  const isOkTax = modalityTax(customerPayload);
+  const isClassOk = consumptionClasses(customerPayload);
+  const isTaxOk = modalityTax(customerPayload);
+  const isConsumpOk = minConsumption(customerPayload);
 
-  const nOkTax = 'Modalidade tarifária não aceita'
-  const nOkClass = 'Classe de consumo não aceita'
+  const nOkTax = 'Modalidade tarifária não aceita';
+  const nOkClass = 'Classe de consumo não aceita';
+  const nOkConsump = 'Consumo muito baixo para tipo de conexão';
 
-  //se a classe de consumo e a taxa tarifária estão OK, retorna falso para 'is refused'
-  if (isOkClasses && isOkTax) return false;
-  if (!isOkClasses && isOkTax) return [nOkClass];
-  if (isOkClasses && !isOkTax) return [nOkTax];
+  //se a classe de consumo e a taxa tarifária estão OK,
+  // retorna falso para 'is refused'
+  if (isClassOk && isTaxOk && isConsumpOk) return false;
 
-  return [nOkClass, nOkTax];
+  //se a classe de consumo não estiver OK:
+  if (!isClassOk && isTaxOk && isConsumpOk) return [nOkClass];
+
+  //se a modalidade tarifária não estiver OK: 
+  if (isClassOk && !isTaxOk && isConsumpOk) return [nOkTax];
+
+  //se o consumo minimo não estiver OK:
+  if (isClassOk && isTaxOk && !isConsumpOk) return [nOkConsump];
+
+  //se a classe de consumo e a modalidade tarifaria não estiverem OK
+  //mas o consumo minimo sim.
+  if (!isClassOk && !isTaxOk && isConsumpOk) return [nOkClass, nOkTax];
+
+  //se a classe de consumo e o consumo minimo não estiverem OK
+  //mas a modalidade tarifaria sim.
+  if (!isClassOk && isTaxOk && !isConsumpOk) return [nOkClass, nOkConsump];
+
+  //se a modalidade tarifaria e o consumo minimo não estiverem OK
+  //mas a classe de consumo sim.
+  if (isClassOk && !isTaxOk && !isConsumpOk) return [nOkTax, nOkConsump];
+
+  return [nOkClass, nOkTax, nOkConsump];
 };
 
 module.exports = {
