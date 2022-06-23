7a7a672fc4c48bb6e9ca8794cd9bef82cb7eafe7
gustacaru <gustavoalmeida26@hotmail.com>
2022-06-23 16:05:08 -0300

    add second unit test

---
+++ src/tests/unitTests/mocks/customersNOkMock.js
@@ -0,0 +1,62 @@
+module.exports = {
+  classeDeConsumoClient: {
+    "numeroDoDocumento": "14041737706",
+    "tipoDeConexao": "bifasico",
+    "classeDeConsumo": "poderPublico",
+    "modalidadeTarifaria": "convencional",
+    "historicoDeConsumo": [
+      3878,
+      9760,
+      5976,
+      2797,
+      2481,
+      5731,
+      7538,
+      4392,
+      7859,
+      4160,
+      6941,
+      4597
+    ]
+  },
+  modalidadeTarifariaCustomer: {
+    "numeroDoDocumento": "14041737706",
+    "tipoDeConexao": "bifasico",
+    "classeDeConsumo": "residencial",
+    "modalidadeTarifaria": "azul",
+    "historicoDeConsumo": [
+      3878,
+      9760,
+      5976,
+      2797,
+      2481,
+      5731,
+      7538,
+      4392,
+      7859,
+      4160,
+      6941,
+      4597
+    ]
+  },
+  tipoDeConexaoCustomer: {
+    "numeroDoDocumento": "14041737706",
+    "tipoDeConexao": "trifasico",
+    "classeDeConsumo": "industrial",
+    "modalidadeTarifaria": "convencional",
+    "historicoDeConsumo": [
+      78,
+      60,
+      76,
+      97,
+      81,
+      31,
+      38,
+      92,
+      59,
+      60,
+      41,
+      97
+    ]
+  },
+}
--- src/tests/unitTests/validationFunct.test.js
+++ src/tests/unitTests/validationFunct.test.js
@@ -1,18 +1,25 @@
 const functs = require('../../api/utils/validationFunct');
 const customersOk = require('./mocks/customersOkMock');
-
+const customersNOk = require('./mocks/customersNOkMock');
 
 test('Testa função \'eligibilityCriteria\' recebendo clientes com parâmetros elegíveis', () => {
-  let response = Object.values(customersOk)
-    .map((customer) => functs
-      .eligibilityCriteria(customer));
-
-  /**testa com 3 casos diferentes */
-  expect(response[0]).toEqual([]);
-  expect(response[1]).toEqual([]);
-  expect(response[2]).toEqual([]);
-});
+    let response = Object.values(customersOk)
+      .map((customer) => functs
+        .eligibilityCriteria(customer));
+  
+    /**testa com 3 casos diferentes */
+    expect(response[0]).toEqual([]);
+    expect(response[1]).toEqual([]);
+    expect(response[2]).toEqual([]);
+  });
 
 test('Testa função \'eligibilityCriteria\' recebendo clientes com parâmetros inelegíveis', () => {
+  let response = Object.values(customersNOk)
+  .map((customer) => functs
+    .eligibilityCriteria(customer));
 
+  /**testa com 3 casos diferentes */
+  expect(response[0]).toContain("Classe de consumo não aceita");
+  expect(response[1]).toContain("Modalidade tarifária não aceita");
+  expect(response[2]).toContain("Consumo muito baixo para tipo de conexão");
 });
